df7505aec425be636c0193069b22fd84f0bf1d5a
CodewithAli45 <ali.my0304@gmail.com>
2025-09-24 23:37:24 +0530

    deployed

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
+
 from flask import Flask
 from routes.main_routes import main
 import os
 
-app = Flask(__name__)
 def create_app():
-    
-    app.config['SECRET_KEY'] = 'your_secret_key_here' # For security, change this
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'change_this_secret')
     app.register_blueprint(main)
     return app
 
+# Expose app for Gunicorn
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
